8e3b1ec29f0b5d3c2d734372c3326b2a562094fa
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2025-03-16 01:29:34 +0600

    add show details

--- script/index.js
+++ script/index.js
@@ -1,9 +1,9 @@
 const removeClasslist = () => {
   const activeButtons = document.getElementsByClassName("active");
-  for(let btn of activeButtons){
+  for (let btn of activeButtons) {
     btn.classList.remove("active");
   }
-}
+};
 function loadCatagories() {
   // fetch the data
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
@@ -19,7 +19,7 @@ function loadVedio() {
       removeClasslist();
       const btnAll = document.getElementById("btn-all");
       btnAll.classList.add("active");
-      displayVedios(data.videos)
+      displayVedios(data.videos);
     });
 }
 const loadCatagoriesVidos = (id) => {
@@ -30,8 +30,33 @@ const loadCatagoriesVidos = (id) => {
       removeClasslist();
       const clickedButton = document.getElementById(`btn-${id}`);
       clickedButton.classList.add("active");
-      displayVedios(data.category)
+      displayVedios(data.category);
     });
+};
+const loadVideoDetails = (id) => {
+  const url = `https://openapi.programming-hero.com/api/phero-tube/video/${id}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => displayVideoDetails(data.video));
+};
+const displayVideoDetails = (video) => {
+  console.log(video);
+  document.getElementById("video_details").showModal();
+  const detailsContainer = document.getElementById("details_container");
+  detailsContainer.innerHTML = `
+  <div class="card bg-base-100 image-full w-96 shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p class="text-sm text-gray-400 flex gap-1 my-2">${video.authors[0].profile_name} <img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt=""></p>
+                        <p class="text-sm text-gray-400 ">${video.others.views} views</p>
+  </div>
+</div>
+  `;
 };
 // {category_id: '1003', category: 'Comedy'}
 function displayCatagories(categories) {
@@ -80,6 +105,7 @@ const displayVedios = (videos) => {
                         <p class="text-sm text-gray-400 ">${video.others.views} views</p>
                     </div>
                 </div>
+                <button onclick="loadVideoDetails('${video.video_id}')" class="btn btn-block">Show details</button>
               </div>
         `;
     videoContainer.appendChild(div);
